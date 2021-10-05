c507c04a2a500f4d35a51abb00e3eb323a0d0672
RamanSlinka <romuch1986@gmail.com>
2021-10-05 22:08:35 +0300

    add video in App
    (TypeError: Cannot read property 'file' of undefined - component "ProfileInfo" when add image)
    ( stay bugs in ProfileContainer : ts-ignore in 14str)

--- src/components/News/News.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import  n  from './News.module.css'
-export default function News() {
-    return (
-        <div>
-            News
-        </div>
-    );
-}
---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import  n  from './Video.module.css'
+export default function Video() {
+    return (
+        <div className={n.wrapper} >
+
+            <iframe width="960" height="515"
+                    src="https://www.youtube.com/embed/0mVbNp1ol_w"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+        </div>
+    );
+}
