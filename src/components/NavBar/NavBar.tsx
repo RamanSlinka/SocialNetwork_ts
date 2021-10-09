b92178f91ee304fba6dd2d05e11e3827a7a08a4d
RamanSlinka <romuch1986@gmail.com>
2021-10-09 17:04:14 +0300

    added icon (ant design) in navbar
    ( stay bugs in ProfileContainer : ts-ignore in 14str)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,13 @@
+import {
+    HomeOutlined,
+    MessageOutlined,
+    SettingOutlined,
+    SoundOutlined,
+    UserOutlined,
+    VideoCameraOutlined
+} from '@ant-design/icons';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import style from './NavBar.module.scss'
 
 
@@ -7,22 +15,40 @@ export default function NavBar() {
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <NavLink to='/profile' activeClassName={style.active}> Profile </NavLink>
+                <div >
+                    <HomeOutlined className={style.itemWrapper}/>
+                    <NavLink to='/profile' activeClassName={style.active}> Profile </NavLink>
+                </div>
             </div>
             <div className={style.item}>
-                <NavLink to='/dialogs' activeClassName={style.active}>Messages</NavLink>
+                <div>
+                    <MessageOutlined  className={style.itemWrapper}/>
+                    <NavLink to='/dialogs' activeClassName={style.active}>Messages</NavLink>
+                </div>
             </div>
             <div className={style.item}>
-                <NavLink to='/users' activeClassName={style.active}>Users</NavLink>
-            </div>
-            <div className={`${style.item} ${style.active}`}>
-                <NavLink to='/video' activeClassName={style.active}>Video</NavLink>
-            </div>
-            <div className={style.item}>
-                <NavLink to='/music'activeClassName={style.active}>Music</NavLink>
+                <div >
+                    <UserOutlined className={style.itemWrapper}/>
+                    <NavLink to='/users' activeClassName={style.active}>Users</NavLink>
+                </div>
             </div>
             <div className={style.item}>
-                <NavLink to='/settings' activeClassName={style.active}>Settings</NavLink>
+                <div >
+                    <VideoCameraOutlined className={style.itemWrapper}/>
+                    <NavLink to='/video' activeClassName={style.active}>Video</NavLink>
+                </div>
+            </div>
+            <div className={style.item}>
+                <div >
+                    <SoundOutlined className={style.itemWrapper}/>
+                    <NavLink to='/music' activeClassName={style.active}>Music</NavLink>
+                </div>
+            </div>
+            <div className={style.item}>
+                <div >
+                    <SettingOutlined className={style.itemWrapper}/>
+                    <NavLink to='/settings' activeClassName={style.active}>Settings</NavLink>
+                </div>
             </div>
         </nav>
     );
